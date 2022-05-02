0975c0f69cc5ef51c1d70833d0547faf0e463a6d
Darrell-H <Darrell-H>
2022-05-02 13:23:53 -0400

    Test

--- Volta Core/Source/Events/Event.h
+++ Volta Core/Source/Events/Event.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "../Core/VoltaCore.h"
+
+namespace Volta {
+
+	enum class EventType
+	{
+
+	};
+
+	class VOLTA_API Event
+	{
+	public:
+		EventType m_Type;
+		EventHandler m_handleEvent;
+
+	};
+
+}
